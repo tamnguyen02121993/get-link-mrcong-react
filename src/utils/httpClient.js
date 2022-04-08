f1dc79d74c59cdfb84f599607c4c6e7af62c41f7
tamnguyen <tamnguyen02121993@gmail.com>
2022-04-08 11:04:25 +0700

    Add .env file

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_APP_SERVER,
+  baseURL: import.meta.env.VITE_APP_API_ENDPOINT,
 });
 
 httpClient.interceptors.request.use(
